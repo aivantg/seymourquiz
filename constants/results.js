d3673b0b5bda90e57f0c2a35ead8d4569ade3b14
Aivant Goyal <aivant.developer@gmail.com>
2025-05-13 00:04:00 -0700

    initial messages

--- constants/results.js
+++ constants/results.js
@@ -1,23 +1,23 @@
 export const results = {
   grace: {
-    description: "you are",
+    description: "you are a hard-working phd student, an incredible singer, and one of the most skilled bakers this side of divisadero.",
     image: "grace.jpeg",
     emote: "grace-emote.png",
     baby: "grace-baby.jpeg"
   },
   drew: {
-    description: "you are ",
+    description: "you are a refreshingly kind, genuine, and nerdy old soul with an uncanny ability to spread your enthusiasm for a topic to anyone who asks.",
     image: "drew.jpeg",
     emote: "drew-emote.png",
   },
   aivant: {
-    description: "you are",
+    description: "you are too lazy to describe yourself right now because it's late and you should go to bed.",
     image: "aivant.jpeg",
     emote: "aivant-emote.png",
     baby: "aivant-baby.jpg"
   },
   jack: {
-    description: "you are",
+    description: "you are a certified HackerMan, using your incredible powers for good (probably). You are the wittiest of the crew and have locked the top spot on the board game leaderboard.",
     image: "jack.jpeg",
     emote: "jack-emote.png",
     baby: "jack-baby.png"
